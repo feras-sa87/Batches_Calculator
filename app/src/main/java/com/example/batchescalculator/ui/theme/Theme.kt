76578c209e3ec1d5c3b7ab9dee707f38274d2d98
FerasSahloul <feras.sa87@gmail.com>
2024-01-26 23:19:56 -0500

    Add dynamic colors - status bar color

--- app/src/main/java/com/example/batchescalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/example/batchescalculator/ui/theme/Theme.kt
@@ -1,10 +1,19 @@
 package com.example.batchescalculator.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -73,12 +82,31 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun BatchesCalculatorTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
-    val colors = if (!darkTheme) {
-        LightColors
-    } else {
-        DarkColors
+    val context = LocalContext.current
+
+    val colors = when {
+        (dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (darkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
